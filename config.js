f044ed4012a47d44eafbfa0da043a71da4a9b5ce
Eshan Nimesha <>
2022-09-30 15:15:25 +0530

    env configs changed

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 console.log(process.env.NODE_ENV)
-dotenv.config({ path: process.env.NODE_ENV === 'production' ? './.env' : './dev.env' })
-
+//dotenv.config({ path: process.env.NODE_ENV === 'production' ? './.env' : './dev.env' })
+dotenv.config({  })
 
 export const NODE_ENV = process.env.NODE_ENV
 export const HOST = process.env.HOST 
