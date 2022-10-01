99620a5d7659961a7de12795e4e92a510fd21412
Eshan Nimesha <>
2022-10-01 20:10:13 +0530

    port updated

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
-console.log(process.env.NODE_ENV)
+//console.log(process.env.NODE_ENV)
 //dotenv.config({ path: process.env.NODE_ENV === 'production' ? './.env' : './dev.env' })
 dotenv.config({  })
 
